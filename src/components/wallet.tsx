437b6b27df6fbfa2ea7facd3aeeff2d173fc0a03
Nur <nuralam.rsc@gmail.com>
2025-08-02 04:25:44 +0600

    feat: add color selector for wallet

--- src/components/wallet.tsx
+++ src/components/wallet.tsx
@@ -5,9 +5,13 @@ import { animate, AnimationSequence, motion } from "motion/react";
 import Image from "next/image";
 import { Skeleton } from "./ui/skeleton";
 import { useState } from "react";
+import { cn } from "@/lib/utils";
 
 const Wallet = () => {
   const [clicked, setClicked] = useState(false);
+  const [selectedColor, setSelectedColor] = useState<"black" | "amber">(
+    "black",
+  );
 
   const sequence = {
     default: [
@@ -36,12 +40,29 @@ const Wallet = () => {
 
   return (
     <motion.div className="relative border w-96 h-96 rounded-xl border-neutral-200 shadow-sm bg-neutral-50 p-1">
+      <div className="absolute right-2 top-2 flex gap-1">
+        <button
+          className="rounded-full size-6 bg-neutral-800 cursor-pointer"
+          onClick={() => setSelectedColor("black")}
+          aria-label="black"
+        ></button>
+        <button
+          className="rounded-full size-6 bg-amber-600 cursor-pointer"
+          onClick={() => setSelectedColor("amber")}
+          aria-label="amber"
+        ></button>
+      </div>
+
       <div className="h-full w-full flex justify-center items-center">
         {!device ? (
           <Skeleton className="h-36 w-36 rounded-4xl" />
         ) : (
           <motion.div
-            className="relative w-36 border h-36 rounded-4xl bg-neutral-800 shadow-xl"
+            className={cn(
+              "relative w-36 border h-36 rounded-4xl shadow-xl",
+              selectedColor === "black" && "bg-neutral-800",
+              selectedColor === "amber" && "bg-amber-700 border-amber-600",
+            )}
             onClick={() => {
               setClicked(!clicked);
               onHover(clicked ? "start" : "end");
@@ -50,7 +71,7 @@ const Wallet = () => {
             onHoverEnd={() => onHover("end")}
           >
             <div
-              className="bg-inherit w-full h-16 absolute z-1 bottom-0 rounded-br-4xl rounded-bl-4xl overflow-hidden"
+              className="bg-inherit w-full h-16 absolute z-1 bottom-0 rounded-br-4xl rounded-bl-4xl overflow-hidden bg-blend-multiply"
               style={{
                 clipPath: `path("M0,0 H172 v144 H0 Z ${device === "mobile" ? "M24,0" : "M32,0"} c 10,-0 19,6 24,14 18,27 38,20 50,0 6,-12 10,-15 43,-15 Z")`,
               }}
@@ -59,9 +80,9 @@ const Wallet = () => {
                 className="absolute inset-0"
                 style={{
                   background:
-                    "linear-gradient(to bottom,rgba(69, 69, 69, 0.69),rgba(0,0,0,1)), url('/noise.svg')",
-                  backgroundBlendMode: "multiply",
-                  backgroundSize: "contain",
+                    selectedColor === "black"
+                      ? "linear-gradient(to bottom,rgba(69, 69, 69, 0.69),rgba(0,0,0,1)), url('/noise.svg')"
+                      : "linear-gradient(to bottom,rgba(255, 113, 0, 0.39),rgba(166, 79, 0, 1)), url('/noise.svg')",
                 }}
               ></div>
             </div>
