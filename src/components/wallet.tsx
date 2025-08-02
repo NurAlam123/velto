5d0f03ed9b68bbae592602199287b829df8b688f
Nur <nuralam.rsc@gmail.com>
2025-08-02 23:37:58 +0600

    feat(wallet): add another version with cards

--- src/components/wallet.tsx
+++ src/components/wallet.tsx
@@ -43,7 +43,7 @@ const Wallet = () => {
             <div
               className="bg-inherit w-full h-16 absolute z-1 bottom-0 rounded-br-4xl rounded-bl-4xl overflow-hidden bg-blend-multiply"
               style={{
-                clipPath: `path("M0,0 H172 v144 H0 Z ${device === "mobile" ? "M24,0" : "M32,0"} c 10,-0 19,6 24,14 18,27 38,20 50,0 6,-12 10,-15 43,-15 Z")`,
+                clipPath: `path("M0,0 H174 v144 H0 Z ${device === "mobile" ? "M24,0" : "M32,0"} c 10,-0 19,6 24,14 18,27 38,20 50,0 6,-12 10,-15 43,-15 Z")`,
               }}
             >
               <div
@@ -58,17 +58,42 @@ const Wallet = () => {
             </div>
             <div className="p-2">
               <div className="text-black h-full rounded-3xl relative">
-                <div className="w-full rounded-3xl h-24 bg-amber-400 p-2 absolute bg-gradient-to-r from-rose-500 via-orange-400 to-amber-500 group-hover:-translate-y-[41px] delay-[80ms] group-hover:delay-[0ms] transition-transform duration-100"></div>
-
-                <div className="w-full rounded-3xl h-24 bg-gradient-to-tr from-emerald-600 to-green-400 p-2 absolute top-4 group-hover:-translate-y-[36px] transition-transform duration-100 delay-[40ms] group-hover:delay-[40ms] ease-[[0.33,1,0.68,1]]"></div>
-
-                <div className="w-full  rounded-3xl h-24 bg-neutral-100 p-2 absolute top-8 group-hover:-translate-y-[26px] transition-transform duration-100 delay-[0ms] group-hover:delay-[80ms] ease-[[0.33,1,0.68,1]]">
+                <div className="w-full rounded-3xl h-24 p-2 absolute bg-gradient-to-br from-[#373736] to-[#515151] group-hover:-translate-y-[60px] group-hover:-translate-x-[60px] group-hover:-rotate-[30deg] delay-[80ms] group-hover:delay-[0ms] transition-transform duration-100">
                   <Image
-                    src="/apple.svg"
-                    alt="apple"
-                    width={24}
-                    height={24}
-                    className="size-6 aspect-auto absolute top-1/2 -translate-y-1/2 left-1/2 -translate-x-1/2"
+                    src="/mastercard.svg"
+                    alt="visa card"
+                    width={256}
+                    height={256}
+                    className="absolute aspect-auto w-6 h-4 right-4"
+                  />
+                </div>
+
+                <div className="w-full rounded-3xl h-24 p-2 absolute bg-gradient-to-br from-zinc-400 via-white to-zinc-400 top-4 group-hover:-translate-y-[68px] group-hover:translate-x-[60px] group-hover:rotate-[20deg] transition-transform duration-100 delay-[40ms] group-hover:delay-[40ms] ease-[[0.33,1,0.68,1]] flex justify-center items-center">
+                  <div className="ms-1 relative flex justify-center flex-col items-center w-full h-full">
+                    <Image
+                      src="/amex-logo.svg"
+                      alt="apple"
+                      width={235}
+                      height={15}
+                      className="w-3/4 h-2 -mt-4"
+                    />
+                    <Image
+                      src="/amex.svg"
+                      alt="apple"
+                      width={1024}
+                      height={1024}
+                      className="w-16"
+                    />
+                  </div>
+                </div>
+
+                <div className="w-full rounded-3xl h-24 bg-gradient-to-br from-sky-400 to-blue-700 p-2 absolute top-8 group-hover:-translate-y-[26px] transition-transform duration-100 delay-[0ms] group-hover:delay-[80ms] ease-[[0.33,1,0.68,1]]">
+                  <Image
+                    src="/visa.svg"
+                    alt="visa card"
+                    width={250}
+                    height={81}
+                    className="absolute aspect-auto w-6 h-4 right-4"
                   />
                 </div>
               </div>
