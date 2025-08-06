78664ce625af6139747f23021b628d029c752eb7
Nur <nuralam.rsc@gmail.com>
2025-08-06 22:49:43 +0600

    style(gabor-project): add elestic easing effect, add scaling hover transition

--- src/components/segments/GaborProject.tsx
+++ src/components/segments/GaborProject.tsx
@@ -1,23 +1,35 @@
+"use client";
+
+import Image from "next/image";
+
 const GaborProject = () => {
   return (
     <div className="w-full">
-      <p className="text-4xl mb-4 font-bold text-neutral-600 h-12 text-center">
+      <p className="text-6xl mb-4 font-bold text-neutral-600 h-12 text-center">
         PROJECT
       </p>
-      <div>
-        <div className="flex flex-col justify-center items-center gap-2">
-          <div className="relative w-full flex flex-col justify-center items-center">
-            <p className="text-center text-2xl font-bold transition-all w-fit duration-150 hover:transition-all hover:duration-150 text-neutral-500 hover:text-neutral-800 peer cursor-pointer select-none hover:scale-y-110">
-              PROJECT 1
+      <div className="mt-12">
+        <div className="flex flex-col justify-center items-center">
+          <div className="relative w-full flex flex-col justify-start items-center">
+            <p className="text-center contain-paint text-3xl/[1em] font-bold transition-[scale] duration-700 w-fit text-neutral-800/50 hover:text-neutral-800 cursor-pointer select-none scale-y-110 hover:scale-y-[120%] ease-elastic uppercase peer">
+              Burn
             </p>
             <div className="hidden peer-hover:block">
-              <div className="absolute w-24 h-24 bg-neutral-400 rounded-xl -top-8 left-6 -rotate-12"></div>
-              <div className="absolute w-24 h-24 bg-neutral-400 rounded-xl -top-36 left-1/2 rotate-6"></div>
-              <div className="absolute w-24 h-24 bg-neutral-400 rounded-xl top-2 right-6 rotate-12"></div>
+              <div className="absolute w-24 h-24 bg-neutral-400 rounded-xl -top-8 left-2 -rotate-12 overflow-hidden border border-neutral-200 shadow-md">
+                <Image
+                  src="/gabor/burn-1.jpg"
+                  alt="first bun"
+                  width={96}
+                  height={96}
+                  className="object-cover w-full"
+                />
+              </div>
+              <div className="absolute w-24 h-24 bg-neutral-400 rounded-xl -top-28 left-1/2 rotate-6"></div>
+              <div className="absolute w-24 h-24 bg-neutral-400 rounded-xl top-2 right-2 rotate-12"></div>
             </div>
           </div>
           <div className="relative w-full flex flex-col justify-center items-center">
-            <p className="text-center text-2xl font-bold transition-all w-fit duration-150 hover:transition-all hover:duration-150 text-neutral-500 hover:text-neutral-800 peer cursor-pointer select-none hover:scale-y-110">
+            <p className="text-center contain-paint text-3xl/[1em] font-bold transition-[scale] duration-700 w-fit text-neutral-800/50 hover:text-neutral-800 cursor-pointer select-none scale-y-110 hover:scale-y-[120%] ease-elastic uppercase peer">
               PROJECT 2
             </p>
             <div className="hidden peer-hover:block">
@@ -27,13 +39,13 @@ const GaborProject = () => {
             </div>
           </div>
           <div className="relative w-full flex flex-col justify-center items-center">
-            <p className="text-center text-2xl font-bold transition-all w-fit duration-150 hover:transition-all hover:duration-150 text-neutral-500 hover:text-neutral-800 peer cursor-pointer select-none hover:scale-y-110">
+            <p className="text-center contain-paint text-3xl/[1em] font-bold transition-[scale] duration-700 w-fit text-neutral-800/50 hover:text-neutral-800 cursor-pointer select-none scale-y-110 hover:scale-y-[120%] ease-elastic uppercase peer">
               PROJECT 3
             </p>
             <div className="hidden peer-hover:block">
-              <div className="absolute w-24 h-24 bg-neutral-400 rounded-xl top-6 left-8 -rotate-12"></div>
+              <div className="absolute w-24 h-24 bg-neutral-400 rounded-xl -top-10 left-4 -rotate-12"></div>
               <div className="absolute w-24 h-24 bg-neutral-400 rounded-xl -top-36 left-32 rotate-6"></div>
-              <div className="absolute w-24 h-24 bg-neutral-400 rounded-xl -top-8 right-6 rotate-12"></div>
+              <div className="absolute w-24 h-24 bg-neutral-400 rounded-xl -top-12 right-6 rotate-12"></div>
             </div>
           </div>
         </div>
