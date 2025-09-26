64f0cd14bc76dc08bb055fa02db17ab86b9b95be
Nur <nuralam.rsc@gmail.com>
2025-09-27 00:08:08 +0600

    refactor(filter-disclouser): use context and provider
    refactor(filter-disclouser): change the design pattern
    
    style(filter-disclouser): add slide-in animaition into global.css

--- src/components/segments/FilterDisclouser.tsx
+++ src/components/segments/FilterDisclouser.tsx
@@ -7,12 +7,19 @@ import {
   ScheduledIcon,
 } from "@/assets/icons";
 import clsx from "clsx";
-import React, { useState } from "react";
+import React, { useContext, useState } from "react";
 
 type OptionsType = {
   title: string;
   icon: React.ElementType;
 };
+
+interface FilterContextType {
+  expend: boolean;
+  setExpend: React.Dispatch<React.SetStateAction<boolean>>;
+  checked: number;
+  setChecked: React.Dispatch<React.SetStateAction<number>>;
+}
 
 const options: OptionsType[] = [
   {
@@ -23,58 +30,85 @@ const options: OptionsType[] = [
     title: "Creation",
     icon: ScheduledIcon,
   },
+  {
+    title: "Creation",
+    icon: ScheduledIcon,
+  },
+  {
+    title: "Creation",
+    icon: ScheduledIcon,
+  },
 ];
+
+// Context & Provider
+const FilterContext = React.createContext<FilterContextType | undefined>(
+  undefined,
+);
 
 const FilterDisclouser = () => {
   const [expend, setExpend] = useState<boolean>(false);
   const [checked, setChecked] = useState<number>(0);
 
   return (
-    <div>
+    <FilterContext.Provider value={{ checked, setChecked, expend, setExpend }}>
       <div className="flex items-center gap-2 relative justify-center">
-        <button
-          className="bg-neutral-50 p-2 size-14 flex items-center justify-center rounded-full shadow-xs cursor-pointer active:scale-[98%] z-[1] border-2 border-neutral-200"
-          onClick={() => setExpend(true)}
-        >
-          <ListFilterIcon className="size-6" />
-        </button>
-
-        <div className="bg-neutral-50 border-2 border-neutral-200 size-14 rounded-full flex items-center justify-center z-0 absolute top-0 left-full inset-0 -translate-x-4">
-          <div className="opacity-60">
-            {React.createElement(options[checked].icon, {
-              className: "size-6",
-            })}
-          </div>
-        </div>
+        <FilterDisclouser.FilterButton />
+        <FilterDisclouser.FilterIcon />
 
         {/* Expended Div */}
-        <FilterDisclouser.ExpendDiv
-          expend={expend}
-          setExpend={setExpend}
-          checked={checked}
-          setChecked={setChecked}
-        />
+        <FilterDisclouser.ExpendDiv />
+      </div>
+    </FilterContext.Provider>
+  );
+};
+
+FilterDisclouser.FilterButton = function FilterDisclouserFilterButton() {
+  const context = useContext(FilterContext);
+  if (!context)
+    throw new Error("FilterButton must be used inside FilterDisclouser");
+
+  const { setExpend } = context;
+
+  return (
+    <button
+      className="bg-neutral-50 p-2 size-14 flex items-center justify-center rounded-full shadow-xs cursor-pointer active:scale-[98%] z-[1] border-2 border-neutral-200"
+      onClick={() => setExpend(true)}
+    >
+      <ListFilterIcon className="size-6" />
+    </button>
+  );
+};
+
+FilterDisclouser.FilterIcon = function FilterDisclouserFilterIcon() {
+  const context = useContext(FilterContext);
+  if (!context)
+    throw new Error("FilterIcon must be used inside FilterDisclouser");
+
+  const { checked } = context;
+
+  return (
+    <div className="bg-neutral-50 border-2 border-neutral-200 size-14 rounded-full flex items-center justify-center z-0 absolute top-0 left-full inset-0 -translate-x-4">
+      <div className="opacity-60">
+        {React.createElement(options[checked].icon, {
+          className: "size-6",
+        })}
       </div>
     </div>
   );
 };
 
-FilterDisclouser.ExpendDiv = function FilterDisclouserExpendDiv({
-  expend,
-  setExpend,
-  checked,
-  setChecked,
-}: {
-  expend: boolean;
-  setExpend: React.Dispatch<React.SetStateAction<boolean>>;
-  checked: number;
-  setChecked: React.Dispatch<React.SetStateAction<number>>;
-}) {
+FilterDisclouser.ExpendDiv = function FilterDisclouserExpendDiv() {
+  const context = useContext(FilterContext);
+  if (!context)
+    throw new Error("ExpendDiv must be used inside FilterDisclouser");
+
+  const { expend } = context;
+
   return (
     <div
-      key={expend ? "edit-badge-expand" : "edit-badge-shrink"}
+      key={expend ? "filter-disclouser-expand" : "filter-disclouser-shrink"}
       className={clsx(
-        "absolute bg-neutral-50 rounded-full p-6 opacity-0 min-w-14 min-h-14 border border-neutral-200 shadow-sm z-[2]",
+        "absolute bg-neutral-50 rounded-full p-6 opacity-0 min-w-14 min-h-14 border border-neutral-200 shadow-sm z-[2] overflow-hidden",
         !expend && "pointer-events-none",
       )}
       style={
@@ -101,11 +135,8 @@ FilterDisclouser.ExpendDiv = function FilterDisclouserExpendDiv({
       <div>
         {options.map((option, i) => (
           <FilterDisclouser.RadioButton
-            key={`filter-disclouser-option-${i}`}
+            key={`filter-disclouser-option-x-${i}`}
             index={i}
-            checked={checked}
-            setChecked={setChecked}
-            setExpend={setExpend}
             title={option.title}
             icon={option.icon}
           />
@@ -119,27 +150,40 @@ FilterDisclouser.RadioButton = function FilterDisclouserRadioButton({
   title,
   icon: Icon,
   index,
-  checked,
-  setChecked,
-  setExpend,
   ...props
 }: {
   title: string;
   icon: React.ElementType;
   index: number;
-  checked: number;
-  setChecked: React.Dispatch<React.SetStateAction<number>>;
-  setExpend: React.Dispatch<React.SetStateAction<boolean>>;
 } & React.HTMLAttributes<HTMLDivElement>) {
+  const context = useContext(FilterContext);
+  if (!context)
+    throw new Error("RadioButton must be used inside FilterDisclouser");
+
+  const { expend, setExpend, checked, setChecked } = context;
+
   return (
     <div
-      className="flex items-center justify-between cursor-pointer py-2"
+      className="flex items-center justify-between py-2 opacity-0 select-none"
       {...props}
       role="presentation"
       onClick={() => {
         setChecked(index);
         setExpend(false);
       }}
+      style={
+        {
+          "--slide-up-transform-start": "translateY(16px) scale(0.95)",
+          "--slide-up-transform-end": "translateY(0px) scale(1)",
+
+          animationName: "slide-up",
+          animationDirection: expend ? "normal" : "reverse",
+          animationDuration: "150ms",
+          animationFillMode: "forwards",
+          animationDelay: expend ? `${index * 150}ms` : "0s",
+          animationTimingFunction: "ease-in-out",
+        } as React.CSSProperties
+      }
     >
       <div className="flex gap-2">
         <Icon className="size-6" />
