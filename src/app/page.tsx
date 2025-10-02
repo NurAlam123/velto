850cd7aa1c13cd3e5271ddb5fb1c715c70bb368e
Nur <nuralam.rsc@gmail.com>
2025-10-02 21:12:25 +0600

    refactor: move right-arrow icon to assets/icons

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 import Box from "@/components/ui/box";
-import { SVGProps } from "react";
 import { interactions } from "@/constants";
 import Image from "next/image";
+import { RightArrow } from "@/assets/icons";
 
 export default function Home() {
   return (
@@ -72,23 +72,3 @@ export default function Home() {
     </div>
   );
 }
-
-const RightArrow = (props: SVGProps<SVGSVGElement>) => {
-  return (
-    <svg
-      xmlns="http://www.w3.org/2000/svg"
-      width="24"
-      height="24"
-      viewBox="0 0 24 24"
-      fill="none"
-      stroke="currentColor"
-      strokeWidth="2"
-      strokeLinecap="round"
-      strokeLinejoin="round"
-      {...props}
-    >
-      <path d="M18 8L22 12L18 16" />
-      <path d="M2 12H22" />
-    </svg>
-  );
-};
