fb9fe34a86c8567bc9cc069dacbd1d602e1db7b8
Nur <nuralam.rsc@gmail.com>
2025-10-06 23:32:09 +0600

    feat(hold-to-disconnect): add reverse animation
    fix(hold-to-disconnect): change the duration
    
    style(hold-to-disconnect): show only storke when mouse release

--- src/components/segments/HoldToDisconnect.tsx
+++ src/components/segments/HoldToDisconnect.tsx
@@ -5,32 +5,38 @@ import { cn } from "@/lib/utils";
 import { useCallback, useRef, useState } from "react";
 
 const HoldToDisconnect = () => {
-  const [show, setShow] = useState(false);
+  const [showStroke, setShowStroke] = useState(false);
+  const [showShadow, setShowShadow] = useState(false);
   const isHolding = useRef<boolean>(false);
 
-  const holdDurationInSec = 2;
+  const holdDuration = 750;
   const perimeter = 240; // perimeter = 2 * (width + height)
   const startTime = useRef<number | null>(null);
   const requestRef = useRef<number>(null);
   const rectRef = useRef<HTMLDivElement | null>(null);
 
-  const handleMouseEnter = () => {
+  const [progress, setProgress] = useState(0);
+
+  const handleMouseDown = () => {
+    if (requestRef.current) cancelAnimationFrame(requestRef.current);
+
     if (rectRef.current)
       rectRef.current.style.setProperty("--progress", `${perimeter}`);
 
     isHolding.current = true;
     startTime.current = null;
-    setShow(true);
+    setShowShadow(true);
+    setShowStroke(true);
     requestRef.current = requestAnimationFrame(animate);
   };
 
-  const handleMouseLeave = () => {
-    isHolding.current = false;
-    setShow(false);
+  const handleMouseUp = () => {
+    if (requestRef.current) cancelAnimationFrame(requestRef.current);
 
-    if (requestRef.current) {
-      cancelAnimationFrame(requestRef.current);
-    }
+    isHolding.current = false;
+    startTime.current = null;
+    setShowShadow(false);
+    requestRef.current = requestAnimationFrame(animate);
   };
 
   const animate = useCallback(
@@ -39,21 +45,30 @@ const HoldToDisconnect = () => {
       if (!startTime.current) startTime.current = timestamp;
 
       const elapsed = timestamp - startTime.current;
-      const percent = Math.min(elapsed / (holdDurationInSec * 1000), 1);
+      const delta = elapsed / holdDuration;
+      const newProgress = isHolding.current
+        ? Math.min(progress + delta, 1)
+        : Math.max(progress - delta, 0);
 
+      setProgress(newProgress);
       rectRef.current.style.setProperty(
         "--progress",
-        `${perimeter * (1 - percent)}`,
+        `${perimeter * (1 - newProgress)}`,
       );
 
-      if (isHolding.current && percent < 1) {
-        requestAnimationFrame(animate);
+      if (
+        (isHolding.current && newProgress < 1) ||
+        (!isHolding.current && newProgress > 0)
+      ) {
+        requestRef.current = requestAnimationFrame(animate);
+      } else if (newProgress >= 1) {
+        // do something on complete
+        console.log("Completed");
+      } else if (newProgress <= 0) {
+        setShowStroke(false);
       }
-
-      // do something on complete
-      if (percent >= 1) console.log("Completed");
     },
-    [isHolding.current, rectRef.current],
+    [progress],
   );
 
   return (
@@ -63,9 +78,9 @@ const HoldToDisconnect = () => {
           "rounded-2xl p-4 bg-neutral-200 shadow-xs w-14 h-14 z-[2] relative outline-none transition-all duration-75 ease-in-out",
           isHolding.current && "scale-[0.95] bg-red-200",
         )}
-        onMouseDown={handleMouseEnter}
-        onMouseUp={handleMouseLeave}
-        onMouseLeave={handleMouseLeave}
+        onMouseDown={handleMouseDown}
+        onMouseUp={handleMouseUp}
+        onMouseLeave={handleMouseUp}
       >
         <UnlinkIcon
           className={cn(
@@ -79,19 +94,21 @@ const HoldToDisconnect = () => {
         ref={rectRef}
         className="absolute top-1/2 left-1/2 -translate-1/2 w-16 h-16 z-[1]"
       >
-        {show && (
+        {showStroke && (
           <svg width="64" height="64" viewBox="0 0 64 64">
-            <rect
-              width="60"
-              height="60"
-              x="2"
-              y="2"
-              fill="none"
-              rx="18"
-              ry="18"
-              strokeWidth="2"
-              className="stroke-red-200"
-            />
+            {showShadow && (
+              <rect
+                width="60"
+                height="60"
+                x="2"
+                y="2"
+                fill="none"
+                rx="18"
+                ry="18"
+                strokeWidth="2"
+                className="stroke-red-200"
+              />
+            )}
             <rect
               style={{
                 strokeDashoffset: "var(--progress)",
