3c7bcbe48ce39a2e783205e789b7e640eaa79390
Nur <nuralam.rsc@gmail.com>
2025-11-10 21:46:38 +0600

    fix(depth-card): fix the depth style using insights form twitter

--- src/components/segments/DepthCard.tsx
+++ src/components/segments/DepthCard.tsx
@@ -2,23 +2,16 @@ const DepthCard = () => {
   return (
     <div className="flex h-screen w-full items-center justify-center">
       <div className="scale-75">
-        <div className="group relative h-96 w-64 perspective-distant transform-3d">
-          <div className="absolute inset-0 -z-10 rounded-2xl bg-neutral-200 opacity-100 transition-all duration-300 ease-out group-hover:top-2 group-hover:right-2 group-hover:-bottom-2 group-hover:-left-1 group-hover:translate-x-2 group-hover:rotate-x-36 group-hover:rotate-z-45 group-hover:opacity-100"></div>
-          <div className="absolute inset-0 -z-10 rounded-2xl border border-neutral-400 bg-white p-4 transition-all duration-300 ease-out group-hover:rotate-x-36 group-hover:rotate-z-45">
+        <div className="group relative h-96 w-64 perspective-distant transform-3d [--card-duration:200ms]">
+          <div className="absolute inset-0 -z-10 rounded-2xl bg-neutral-200 opacity-100 transition-all duration-(--card-duration) ease-out group-hover:top-2 group-hover:right-2 group-hover:-bottom-2 group-hover:-left-1 group-hover:translate-x-2 group-hover:rotate-x-36 group-hover:rotate-z-45 group-hover:opacity-100"></div>
+          <div className="absolute inset-0 -z-10 rounded-2xl border border-neutral-400 bg-white p-4 transition-all duration-(--card-duration) ease-out group-hover:rotate-x-36 group-hover:rotate-z-45 overflow-hidden">
             <p className="text-xl font-medium text-neutral-500">Veltoo</p>
 
-            <div className="absolute inset-0 top-1/2 right-0 h-full -translate-y-1/2 overflow-hidden">
-              <div className="relative left-1/2 h-full perspective-distant transform-3d *:transition-all *:ease-in-out">
-                <div className="absolute top-10 -right-26 size-80 -translate-y-0 -translate-z-20 scale-80 rotate-12 rounded-full bg-neutral-400 duration-335 group-hover:top-1 group-hover:-right-2 group-hover:scale-100"></div>
-
-                <div className="absolute top-6 -right-14 size-76 origin-bottom -translate-y-0 -translate-z-20 scale-85 rotate-12 rounded-full bg-neutral-500 duration-330 group-hover:top-4 group-hover:right-4 group-hover:scale-100"></div>
-
-                <div className="absolute top-10 -right-8 size-72 origin-bottom -translate-y-0 -translate-z-20 scale-90 rotate-6 rounded-full bg-neutral-600 duration-325 group-hover:top-8 group-hover:-right-0 group-hover:scale-100"></div>
-
-                <div className="absolute top-14 -right-2 size-68 origin-bottom -translate-y-0 -translate-z-20 scale-95 rotate-6 rounded-full bg-neutral-700 duration-320 group-hover:top-12 group-hover:right-0 group-hover:scale-100"></div>
-
-                <div className="absolute top-16 right-0 size-64 -translate-y-0 rounded-full bg-neutral-800 opacity-100 duration-300 group-hover:-right-2 group-hover:-translate-z-18"></div>
-              </div>
+            <div className="absolute size-64 perspective-distant transform-3d top-1/2 left-1/2 -translate-y-1/2 -translate-x-4 rounded-full *:rounded-full *:transition-all *:duration-200 *:ease-out *:absolute *:inset-0 *:border-2 *:border-neutral-700 overflow-hidden bg-[repeating-linear-gradient(45deg,var(--color-neutral-400),var(--color-neutral-300)_2px,var(--color-white)_2px,var(--color-white)_4px)]">
+              <div className="group-hover:translate-x-0 group-hover:translate-y-0"></div>
+              <div className="group-hover:translate-x-4 group-hover:translate-y-6"></div>
+              <div className="group-hover:translate-x-8 group-hover:translate-y-12"></div>
+              <div className="group-hover:translate-x-12 group-hover:translate-y-18 bg-neutral-800 "></div>
             </div>
           </div>
         </div>
